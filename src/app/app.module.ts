f4c3e202e0682c373a34162d6c23dc18a0a2aed1
llckumadevelop <kumauser01@kuma.ink>
2024-04-04 15:25:48 +0700

    component-interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent } from './hero-parent.component';
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
+import { Version } from '@angular/compiler';
+import { VersionChildComponent } from './version-child.component';
+import { VersionParentComponent } from './version-parent.component';
+import { VoteTakerComponent } from './votetaker.component';
+import { VoterComponent } from './voter.component';
+import { CountdownTimerComponent } from './countdown-timer.component';
+import { CountdownLocalVarParentComponent } from './countdown-parent.component';
+import { CountdownViewChildParentComponent } from './countdown-parent-vc.component';
+import { MissionControlComponent } from './missioncontrol.component';
+import { AstronautComponent } from './astronaut.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent,
+    HeroParentComponent,
+    NameParentComponent,
+    NameChildComponent,
+    VersionChildComponent,
+    VersionParentComponent,
+    VoteTakerComponent,
+    VoterComponent,
+    CountdownTimerComponent,
+    CountdownLocalVarParentComponent,
+    CountdownViewChildParentComponent,
+    MissionControlComponent,
+    AstronautComponent
   ],
   imports: [
     BrowserModule,
